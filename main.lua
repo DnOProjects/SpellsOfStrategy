bb07ee48589709689e24902932d2dbfd9893bf20
Relton <ori.taylor@gmail.com>
2019-06-25 23:43:50 +0100

    Fixing main

--- main.lua
+++ main.lua
@@ -34,30 +34,7 @@ function love.draw()
 	arrow:draw()
 end
 
-local mouseWasUp = true
-
 function love.update(dt)
-	local mx,my = love.mouse.getX(), love.mouse.getY()
-
-	if love.mouse.isDown(1) then
-		if mouseWasUp then
-			local mapCoords = screenToMap(getMouseCoords())
-			if mapCoords:matches(Characters[turnNum].pos) then
-				Characters[turnNum].beingDragged = true
-			end
-		end
-		mouseWasUp = false
-	else
-		if not mouseWasUp then
-			local dest = screenToMap(getMouseCoords())
-			if Characters[turnNum].beingDragged and dest:distance(Characters[turnNum].pos)==1 and isInBounds(dest) then
-				Characters[turnNum].pos = screenToMap(getMouseCoords())
-				Characters[turnNum].beingDragged = false
-			end
-		end
-		mouseWasUp = true
-	end
-
 	Input.update()
 	UI.update(dt)
 end
